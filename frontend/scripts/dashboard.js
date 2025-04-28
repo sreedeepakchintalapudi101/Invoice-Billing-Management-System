38ab279489a2d2e7a8b35ce15f70ea09e86c9efd
Deepak <deepak.ch@algonox.com>
2025-04-28 18:59:09 +0530

    Updated Frontend

--- frontend/scripts/dashboard.js
+++ frontend/scripts/dashboard.js
@@ -1,40 +1,13 @@
 function showTab(tabName) {
     document.querySelectorAll('.tab-section').forEach(tab => {
-      tab.classList.remove('active');
+      tab.classList.add('hidden');
     });
-    document.getElementById(tabName + '-tab').classList.add('active');
   
-    if (tabName === 'files') {
-      loadIngestedFiles();
-    }
+    document.getElementById(tabName + '-tab').classList.remove('hidden');
   }
   
-  function loadIngestedFiles() {
-    fetch("http://localhost:8081/get_ingested_files", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        user_id: localStorage.getItem("user_id"),
-        user_type: localStorage.getItem("user_type")
-      })
-    })
-    .then(res => res.json())
-    .then(data => {
-      const tbody = document.getElementById("files-tbody");
-      tbody.innerHTML = "";
-      data.data.forEach(file => {
-        tbody.innerHTML += `
-          <tr>
-            <td>${file.file_name}</td>
-            <td>${file.file_type}</td>
-            <td>${file.status}</td>
-            <td>${file.upload_time}</td>
-          </tr>
-        `;
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching ingested files:', error);
-    });
-  }
+  // Show Dashboard tab by default
+  window.onload = function() {
+    showTab('dashboard');
+  };
   
