6f288628d38cb6b3dc6881ff26247be9fdbbe077
Deepak <deepak.ch@algonox.com>
2025-05-01 18:02:44 +0530

    Updated invoice_management.py

--- backend/invoice_management/invoice_management.py
+++ backend/invoice_management/invoice_management.py
@@ -30,7 +30,7 @@ load_dotenv()
 
 
 logging.basicConfig(
-    level=logging.DEBUG
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     headers=[
         logging.StreamHandler()
