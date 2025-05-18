e5dc36e4588ae3d594f022b02c06afab25f4d64f
Deepak <deepak.ch@algonox.com>
2025-05-18 09:39:14 +0530

    Updated bounding_box_detection_api.py in bounding_box_detection_api container

--- backend/bounding_box_detection_api/bounding_box_detection_api.py
+++ backend/bounding_box_detection_api/bounding_box_detection_api.py
@@ -1,31 +1,9 @@
-import flask
-from flask import request, render_template, redirect
-from flask import Flask
-import requests
+from flask import Flask, request
 from flask_cors import CORS
-import base64
-import re
-# import APScheduler
-import os
-import email
-import smtplib
-import imaplib
-import paramiko
-import uuid
-from email import message_from_bytes
-from email.header import decode_header
-import time
-from datetime import datetime
-import threading
-from backend.database.db_utils import get_connection, execute_, update_query, insert_query
-import cv2
-import pytesseract
-from PIL import Image
-import ultralytics
-from huggingface_hub import hf_hub_download
-from doclayout_yolo import YOLOv10
 import logging
-
+import os
+import cv2
+from doclayout_yolo import YOLOv10
 app = Flask(__name__)
 CORS(app)
 
