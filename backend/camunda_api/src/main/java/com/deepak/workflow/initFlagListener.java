18c897c3736bc7a2182a8f236d229617804fc7a9
Deepak <deepak.ch@algonox.com>
2025-07-03 21:26:07 +0530

    Add initFlagListener to initialize flag variable on start event in email ingestion workflow

---
+++ backend/camunda_api/src/main/java/com/deepak/workflow/initFlagListener.java
@@ -0,0 +1,16 @@
+package com.deepak.workflow;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+import java.util.logging.Logger;
+
+public class initFlagListener implements ExecutionListener {
+
+    private final logger logger = logger.logger.getLogger(initFlagListener.class.getName())
+
+    @Override
+    public void notify(DelegateExecution execution) throws Exception {
+        execution.setVariable("flag", false);
+        logger.info("Flag variable initialized to false on start event.");
+    }
+}
